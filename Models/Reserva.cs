2e761c9771a2031db3ad546b267998305e2ec0c9
Joanna Lima <joanna-karla@hotmail.com>
2022-10-17 15:49:24 -0300

    Desafio concluído!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -15,16 +16,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                 throw new Exception("Erro: Capacidade menor que o número de hóspedes recebido");
             }
         }
 
@@ -35,25 +33,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+         return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+           decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor - (valor/10);
+                Console.WriteLine("Desconto de 10% aplicado em hospedagens com mais de 10 dias de reserva");
             }
-
             return valor;
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -12,14 +12,46 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 hospedes.Add(p1);
 hospedes.Add(p2);
 
-// Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine(" ** Desafio: Construindo um sistema de hospedagem de um hotel no C# ** ");
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Quantidade de Hóspedes");
+    Console.WriteLine("2 - Valor Final da Diária");
+    Console.WriteLine("3 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+            break;
+
+        case "2":
+           Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+            break;
+
+        case "3":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
